1fd9d2ae8205b52c179b9d7ed8f9ac985c3847e7
Krishna <thontakrishna1212@gmail.com>
2024-10-09 01:13:07 +0530

    Updated index.js to customize the response

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+    res.send('<h1>Welcome to My Application!</h1><p>This is a Node.js application running on Render.</p>');
 });
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
